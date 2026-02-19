3ef30dd1e358abb9631842bb10053eac52614de5
CS2 Kitchen <iamcs2kitchen@gmail.com>
2026-02-19 21:39:01 +0530

    Update autoD2BenchMark.py
    Fixed map load error due to fps benchmark map update.

--- autoD2BenchMark.py
+++ autoD2BenchMark.py
@@ -1,14 +1,14 @@
 #1. make sure you select your settings first
 #2. restart your computer after selecting required settings
 #3. load up game into main menu dont press anything
-#4. bind your capframex hotkey to f4 keep capframex open before running script
+#4. bind your capframex hotkey to f4 keep capframex open before running script\
 #5. set capframex timer to 105 - 107 seconds.
 #6. make sure it is consistent across all your runs
 #7. select number of runs by changing the variable
 #8. press F6 to start script or press f8 to terminate
 #9. first two cold runs are done to build shaders and to ensure warm caching
 #10. then number of runs are done and all your results are saved to capframex
-#by default number of trials = 10, make sure you change the load time if your pc is slower
+#by default number of trials = 5, make sure you change the load time if your pc is slower
 
 import time
 from pynput.keyboard import Controller, Key, Listener
@@ -18,8 +18,8 @@ keyboard = Controller()
 running = False
 terminate = False  
 
-COMMAND = "map_workshop 3240880604 de_dust2_benchmark"
-LOOPS = 10   # benchmark loops (with F4 bound to capframeX and seconds to 107)
+COMMAND = "map_workshop 3240880604 de_dust2"
+LOOPS = 5   # benchmark loops (with F4 bound to capframeX and seconds to 107)
 
 def press_key(key):
     keyboard.press(key)
@@ -40,13 +40,15 @@ def run_sequence():
     time.sleep(3)
 
     # Compulsory warmup runs without logging capframex
-    for i in range(2):
+    for i in range(1):
         if terminate: break
         print(f" Warmup {i+1}/2 (no F4) ")
 
         press_key('`')
         time.sleep(1.0);  
         if terminate: break
+
+        
 
         type_text(COMMAND)
         time.sleep(1.0);  
@@ -63,6 +65,8 @@ def run_sequence():
         time.sleep(120.0)  
         press_key('`')
         time.sleep(1) 
+        press_key('`')
+        time.sleep(1.0); 
 
     #  Benchmark loops pressing F4 
     for i in range(LOOPS):
@@ -87,6 +91,8 @@ def run_sequence():
 
         press_key(Key.f4)
         time.sleep(120.0); 
+        press_key('`')
+        time.sleep(1.0); 
         if terminate: break
 
         press_key('`')
@@ -117,4 +123,3 @@ print("Press F8 to terminate safely.")
 
 with Listener(on_press=on_press) as listener:
     listener.join()
-
